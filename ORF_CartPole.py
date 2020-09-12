c0e399c968c4851965069f0430fc0213385efeee
minjo101 <joosungm@cedar1.cedar.computecanada.ca>
2020-09-11 23:17:15 -0700

    ORF_CartPole_v2 updated
    Former-commit-id: 1e13c331854e49197b8002f184d213fcee31c432
    Former-commit-id: 7173dd8fdca35a557a3283bd601faa917af03a92

--- ORF_CartPole.py
+++ ORF_CartPole.py
@@ -50,7 +50,7 @@
 # 
 # 
 
-# In[27]:
+# In[4]:
 
 
 import gym
@@ -59,40 +59,50 @@ import pickle
 from collections import deque
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-import os
 import os.path
 import copy
-# from PIL import Image
+from PIL import Image
 # from sklearn.multioutput import MultiOutputRegressor
 # from lightgbm import LGBMRegressor
 import numpy as np
 import time
 import datetime
-import multiprocessing
 
 
-# In[28]:
+# In[113]:
 
 
-import ORF as ORF
+import ORF
 
 
-# In[29]:
+# In[145]:
 
 
 class ORF_DQN(): 
     
-    def __init__(self, n_state, n_action, replay_size, ORFparams):
+    def __init__(self, n_state, n_action, replay_size):
         self.n_action = n_action
         self.a_model = {} # to build RF for each action
-        self.a_params = {a: ORFparams for a in range(n_action)}
         self.isFit = False
-    
+        self.a_state = {} # to contain state for each action
+        self.a_target = {} # to contain q_value for each action
+        self.a_params = {}
+        for a in range(n_action): # To contain separate data for each action
+            self.a_state[a] = []
+            # self.a_target[a] = []
+        
+        # self.rg = [[-255, 255]] * n_action
+        # self.rg = [[-4.8, 4.8], [-2, 2], [-0.2, 0.2], [-3, 3]] # for CartPole
+
     def predict(self, s):            
         # s: (4,) array (for cartpole)
+        # s = np.array(s).reshape(1,-1) # converts (4,) to (1,4) (2 dimensional)
+        preds = []
         
-        preds = [self.a_model[a].predict(s) for a in range(self.n_action)]
+        for a in range(self.n_action):
+            preds.append(self.a_model[a].predicts([s])) # should be (, n_action)
         # print(preds)
+        # print(np.argmax(preds))
         return preds
 
     def gen_epsilon_greedy_policy(self, epsilon, n_action):
@@ -101,7 +111,7 @@ class ORF_DQN():
             # state: (4,) array
             ran = "_"
             q_values =[0.0, 0.0]
-            if np.random.uniform(0,1) < epsilon:
+            if np.random.random() < epsilon:
                 ran = "Random"
                 return([random.randint(0, n_action - 1), ran, q_values])
             else:
@@ -119,28 +129,55 @@ class ORF_DQN():
 
 
     def replay(self, memory, replay_size, gamma):
-        if len(memory) == replay_size: # Build initial models
+        if len(memory) == replay_size:
             
-            for a in range(self.n_action):
-                self.a_params[a]['xrng'] = ORF.dataRange([v[0] for v in memory if v[1] == a])
-                self.a_model[a] = ORF.ORF(self.a_params[a]) # Fit initial RFs for each action            
+            for state, action, *_ in memory:
+                self.a_state[action].append(state)
             
-        if len(memory) >= replay_size: # When the memory size exceeds the replay_size, start updating the RFs
-            
-            replay_data = random.sample(memory, replay_size) # replay_data consists of [state, action, next_state, reward, is_done]
-            for state, action, next_state, reward, is_done in replay_data:
-                
-                q_values = self.predict(state) # (, n_actions)
-                q_values[action] = reward + gamma * np.max(self.predict(next_state)) if is_done == False else -1000 * reward
-                
-                # Update the RF for the action taken
-                xrng = ORF.dataRange([v[0] for v in replay_data if v[1] == action])
-                self.a_model[action].update(state, q_values[action])    
+            for i in range(n_action):
+                self.a_params[i] = {'minSamples': replay_size*2, 'minGain': 0.1, 'xrng': ORF.dataRange(self.a_state[i]), 'maxDepth': 30}
+                self.a_model[i] = ORF.ORF(self.a_params[i], numTrees=30) # Fit initial RFs for each action            
             self.isFit = True
+        
+        if len(memory) >= replay_size: # When the memory size exceeds the replay_size, start updating the RFs
+            replay_data = random.sample(memory, replay_size)
+            # replay_data = memory[len(memory)-1] # draw the latest input
+            # replay_data consists of [state, action, next_state, reward, is_done]
+            
+            # Compute q-values for each state and action
+            for state, action, next_state, reward, is_done in replay_data:
+            # state, action, next_state, reward, is_done = replay_data
+            
+                q_values = self.predict(state) # (, n_actions)
+                # print(q_values)
+                if is_done == False:
+                    q_values_next = self.predict(next_state) # (1,n_action) array
+                    
+                    q_values[action] = reward + gamma * np.max(q_values_next) # float
+                else:
+                    q_values[action] = -1000 * reward
+                
+                # print(q_values)
+                
+                for i in range(len(q_values)):
+                    if type(q_values[i]) == list:
+                        q_values[i] = q_values[i][0]
+                
+                # Update both RFs
+                
+                self.a_model[action].update(state, q_values[action])
+                # print(q_values)
+                # for j in range(self.n_action):
+                #     if j == action:
+                #         self.a_model[action].update(state, q_values[action])
+                #     else:
+                #         # print(q_values[j])
+                #         self.a_model[j].update(state, q_values[j])
+                
             
 
 
-# In[30]:
+# In[146]:
 
 
 def q_learning(env, estimator, n_episode, replay_size, gamma=1.0, epsilon=0.1, epsilon_decay=0.95):
@@ -160,7 +197,7 @@ def q_learning(env, estimator, n_episode, replay_size, gamma=1.0, epsilon=0.1, e
             
             total_reward_episode[episode] += reward
             
-            # ep[episode].append((i, state, ran, action))
+            ep[episode].append((i, state, ran, action))
             memory.append((state, action, next_state, reward, is_done))
             
             if is_done:
@@ -171,52 +208,47 @@ def q_learning(env, estimator, n_episode, replay_size, gamma=1.0, epsilon=0.1, e
         # print(epsilon)
 
 
-# In[33]:
+# In[147]:
 
+
+backup_file_name = "ORF_CartPole_" + time.strftime("%y%m%d") + "_1"
 
 env = gym.envs.make("CartPole-v1")
 n_state = env.observation_space.shape[0]
 n_action = env.action_space.n
 
 memory = deque(maxlen=10000)
-n_episode = 1000
-replay_size = 32
+n_episode = 100
+replay_size = 60
 
-ORFparams = {'minSamples': replay_size*2, 'minGain': 0.1, 'xrng': None, 'maxDepth': 12, 'numTrees': 5 }
-
-dqn = ORF_DQN(n_state, n_action, replay_size, ORFparams) 
+dqn = ORF_DQN(n_state, n_action, replay_size) 
 
 total_reward_episode = [0] * n_episode
 
 start = time.time()
 
-ep = {i: [] for i in range(n_episode)}
+ep = {}
+for i in range(n_episode):
+    ep[i] = []
 
 q_learning(env, dqn, n_episode, replay_size, gamma=1.0, epsilon=0.5, epsilon_decay=0.99) # runs the alg
 
 end = time.time()
 duration = int(end - start)
 
+img_file = backup_file_name + ".jpg"
 print("learning duration =", duration, " seconds")
 print("mean reward = ", np.mean(total_reward_episode))
 print("max reward = ", max(total_reward_episode))
-
-
-# In[23]:
-
-
-backup_file_name = "ORF_CartPole_" + time.strftime("%y%m%d") + "_2"
-img_file = backup_file_name + ".jpg"
 plt.plot(total_reward_episode)
 plt.title("(ORF) Total reward per episode")
 plt.xlabel("Episode")
 plt.ylabel("Total reward")
-plt.hlines(195, xmin=0, xmax=n_episode, linestyles="dotted", colors="gray")
 plt.show()
 plt.savefig(fname = img_file)
 
 
-# In[25]:
+# In[148]:
 
 
 # To back-up the work
@@ -227,7 +259,6 @@ myEnv = dict()
 myEnv["t_r_e"] = total_reward_episode
 myEnv["duration"] = duration
 myEnv["episode_details"] = ep
-myEnv["params"] = ORFparams
 # myEnv["ORF_params"] = params
 
 with open(backup_file, "wb") as file:
@@ -237,50 +268,5 @@ with open(backup_file, "wb") as file:
 # In[ ]:
 
 
-# About Cartpole
-    """
-    Description:
-        A pole is attached by an un-actuated joint to a cart, which moves along
-        a frictionless track. The pendulum starts upright, and the goal is to
-        prevent it from falling over by increasing and reducing the cart's
-        velocity.
-    
-    Source:
-        This environment corresponds to the version of the cart-pole problem
-        described by Barto, Sutton, and Anderson
-    
-    Observation:
-        Type: Box(4)
-        Num     Observation               Min                     Max
-        0       Cart Position             -4.8                    4.8
-        1       Cart Velocity             -Inf                    Inf
-        2       Pole Angle                -0.418 rad (-24 deg)    0.418 rad (24 deg)
-        3       Pole Angular Velocity     -Inf                    Inf
-    
-    Actions:
-        Type: Discrete(2)
-        Num   Action
-        0     Push cart to the left
-        1     Push cart to the right
-        Note: The amount the velocity that is reduced or increased is not
-        fixed; it depends on the angle the pole is pointing. This is because
-        the center of gravity of the pole increases the amount of energy needed
-        to move the cart underneath it
-    
-    Reward:
-        Reward is 1 for every step taken, including the termination step
-    
-    Starting State:
-        All observations are assigned a uniform random value in [-0.05..0.05]
-    
-    Episode Termination:
-        Pole Angle is more than 12 degrees.
-        Cart Position is more than 2.4 (center of the cart reaches the edge of
-        the display).
-        Episode length is greater than 200.
-    
-    Solved Requirements:
-        Considered solved when the average return is greater than or equal to
-        195.0 over 100 consecutive trials.
-    """
+
 
