1369d63efba96c8f7300636ddb2001ce5cb62028
minjo101 <joosungm@cedar1.cedar.computecanada.ca>
2020-09-13 09:07:00 -0700

    ORF updated
    Former-commit-id: b55c3d4e2c1905c302fd224a55837a82614c119b
    Former-commit-id: 2a1d3f9b2f881012a0a1d2b1060011a8184f4968

--- job_cpu.sh
+++ job_cpu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -t 3-00:00
 #SBATCH --account=rrg-lelliott
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=16G
 #SBATCH --mail-user=<joosungm@sfu.ca>
 #SBATCH --mail-type=ALL
 
 module load python/3.7
 source ~/torch/bin/activate
-python "ORF_CartPole.py"
+python "ORF_CartPole_v2.py"
 
