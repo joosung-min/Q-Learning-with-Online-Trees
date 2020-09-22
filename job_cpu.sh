e377b7c9d28a0b1da7b3c60134c673d5df2b50f5
minjo101 <joosungm@cedar1.cedar.computecanada.ca>
2020-09-22 05:06:15 -0700

    ORF_LunarLander updated
    Former-commit-id: 96de940a146cb5a0a6f500c9e3cde4267a1c017b
    Former-commit-id: d689cb72ce93784d1104ce9f11e39a23e9c9c75e

--- job_cpu.sh
+++ job_cpu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -t 3-00:00
 #SBATCH --account=rrg-lelliott
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=16G
 #SBATCH --mail-user=<joosungm@sfu.ca>
 #SBATCH --mail-type=ALL
 
-module load python/3.7
+module load python/3.8
 source ~/torch/bin/activate
-python "ORF_CartPole_v2.py"
+python "ORF_LunarLander_v1.py"
 
