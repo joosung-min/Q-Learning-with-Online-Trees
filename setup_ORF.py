9970d9077407fd48158c829dbcbf6ba3d7781aae
minjo101 <joosungm@cedar1.cedar.computecanada.ca>
2020-10-22 15:56:04 -0700

    orf_mountaincar
    Former-commit-id: 69f0f969e5951f275b13a8c6cd60ec0f4009c48a
    Former-commit-id: 736ab00bab6dcec26256025bb8885898b8e9865f

--- setup_ORF.py
+++ setup_ORF.py
@@ -2,8 +2,7 @@
 #cython: language_level=3, boundscheck=False
 from distutils.core import setup
 from Cython.Build import cythonize
-import numpy
 setup(
-    ext_modules = cythonize("ORF_cython.pyx")
+    ext_modules = cythonize("ORF_py_cython.pyx")
 #    include_dirs=[numpy.get_include()]
 )
