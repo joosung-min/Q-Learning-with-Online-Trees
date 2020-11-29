387feae3e1d9a1e994e5477828842d840dda9936
minjo101 <joosungmin@gmail.com>
2020-11-28 23:13:30 -0800

    org mtcar
    Former-commit-id: 923e062e92bc37c9595626e97b1680ab995cdb9d [formerly 54357b4aef7e4485d1cf787b353452a18712ba35]
    Former-commit-id: 465bf64fca321eec36f14b373c1e0e85807d4d9c
    Former-commit-id: 14cd5a7f671a45c1995534d6e64d087b3ef8af61

--- LunarLander/job_lunar.sh
+++ LunarLander/job_lunar.sh
@@ -2,12 +2,12 @@
 #SBATCH -t 15-00:00
 #SBATCH --account=rrg-lelliott
 #SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-cpu=16G
 #SBATCH --mail-user=<joosungm@sfu.ca>
 #SBATCH --mail-type=ALL
 
 module load python/3.8
 source ~/torch/bin/activate
 python setup_ORF.py build_ext --inplace
-python "ORF_LunarLander_v1.py"
+python "orf_lunar.py"
 
