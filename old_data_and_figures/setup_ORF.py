caa8dc657b5bba79865756a84a7fb611a7d05f5d
minjo101 <joosungmin@gmail.com>
2021-05-18 22:57:17 -0700

    cleaned
    Former-commit-id: 45692c5aca4b656c42fbe8bb2aba0a7f28ea14c4 [formerly e642d4d845debf6be6a92181ba84666408b3741e]
    Former-commit-id: e8f54003f30268be66df0e819d81d8f19532daee
    Former-commit-id: 5775f70c278de2fbe3e5cfe77bc37c1e56777938

--- old_data_and_figures/setup_ORF.py
+++
@@ -1,8 +0,0 @@
-#!python
-#cython: language_level=3, boundscheck=False
-from distutils.core import setup
-from Cython.Build import cythonize
-setup(
-    ext_modules = cythonize("ORF_py_cython.pyx")
-#    include_dirs=[numpy.get_include()]
-)
